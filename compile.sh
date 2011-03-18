131f2fade29c23ac4ad03f626eec7d6665b71aca
Carlos Cabrera <point_to_null@esdebian.org>
2011-03-18 06:47:23 -0300

    Added note comunicado1

--- compile.sh
+++ compile.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+set -e # Modo estricto
+
+rst2html=$(which rst2html) || (
+    echo Necesita python-docutils
+    exit 1 )
+wkhtmltopdf=$(which wkhtmltopdf) || (
+    echo Necesita wkhtmltopdf
+    exit 2 )
+
 input="$@"
 basename=$(basename $input .rst)
 options='--stylesheet=html4css1.css --template=template.txt'
-output="html/$basename.html"
+htmloutput="html/$basename.html"
+pdfoutput="pdf/$basename.pdf"
 
-cat header.rst "$input" footer.rst | rst2html $options > "$output"
+cat header.rst "$input" footer.rst | $rst2html $options > "$htmloutput"
+$wkhtmltopdf "$htmloutput" "$pdfoutput"
