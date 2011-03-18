bcd48ce32227ec646f3eb9e99e44e03e535d7ccb
Carlos Cabrera <point_to_null@esdebian.org>
2011-03-18 06:48:23 -0300

    Ahora crea el fichero pdf y lo sube al repositorio xD

--- compile.sh
+++ compile.sh
@@ -5,9 +5,14 @@ set -e # Modo estricto
 rst2html=$(which rst2html) || (
     echo Necesita python-docutils
     exit 1 )
+
 wkhtmltopdf=$(which wkhtmltopdf) || (
     echo Necesita wkhtmltopdf
     exit 2 )
+
+git=$(which git) || (
+    echo Necesita git
+    exit 3 )
 
 input="$@"
 basename=$(basename $input .rst)
@@ -16,4 +21,9 @@ htmloutput="html/$basename.html"
 pdfoutput="pdf/$basename.pdf"
 
 cat header.rst "$input" footer.rst | $rst2html $options > "$htmloutput"
+
 $wkhtmltopdf "$htmloutput" "$pdfoutput"
+
+git add $input $htmloutput $pdfoutput
+git commit -m "Added note $basename"
+git push
