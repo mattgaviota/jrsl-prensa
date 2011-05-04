6a7de5e830b083a64d0d64591d5ce2b5cf8c3aca
Carlos Cabrera <point_to_null@esdebian.org>
2011-05-04 15:01:14 -0300

    cambiada capitalización para evitar inundar redes sociales con pequeños cambios

--- compile.sh
+++ compile.sh
@@ -29,10 +29,11 @@ input="$@"
     )
 
 basename=$(basename $input .rst)
+compiledrst="rst/$basename-compiled.rst"
 htmloutput="html/$basename.html"
 pdfoutput="pdf/$basename.pdf"
 modifydate=$(stat "$input" | awk '
-    /^Modify/{system("date --date=\"" $2 "\" \"+%A %d de %B de %Y\"")}')
+    /^Modify/{system("date --date=\"" $2 "\" \"+%d de %B de %Y\"")}')
 
 echo "Acortando enlaces url"
 htmlurl="https://github.com/pointtonull/jrsl-prensa/blob/master/$input"
@@ -40,7 +41,7 @@ htmlshorturl=$(echo $htmlurl|shorturl)
 pdfurl="https://github.com/pointtonull/jrsl-prensa/raw/master/$pdfoutput"
 pdfshorturl=$(echo $pdfurl|shorturl)
 
-echo "Generando documento html"
+echo "Compilando rst final"
 rst2htmloptions='--stylesheet=html4css1.css --template=template.txt'
 awk -v pdfshorturl="$pdfshorturl"\
     -v htmlshorturl="$htmlshorturl"\
@@ -51,18 +52,21 @@ awk -v pdfshorturl="$pdfshorturl"\
         gsub("\\|modifydate\\|", modifydate)
         print $0
     }
-' header.rst "$input" footer.rst | $rst2html $rst2htmloptions > "$htmloutput"
+' header.rst "$input" footer.rst > $compiledrst
+
+echo "Generando documento html"
+$rst2html $rst2htmloptions "$compiledrst" > "$htmloutput"
 
 echo "Generando documento pdf"
 wkhtmltopdfoptions="\
-       --margin-bottom 20\
-       --margin-left 20\
-       --margin-right 20\
-       --margin-top 20\
+       --margin-bottom 9\
+       --margin-left 10\
+       --margin-right 10\
+       --margin-top 9\
 "
 $wkhtmltopdf $wkhtmltopdfoptions "$htmloutput" "$pdfoutput"
 
 echo "Publicando documentos en el sitio"
-git add $input $htmloutput $pdfoutput
-git commit -m "Compiled $basename"
+git add $input $compiledrst $htmloutput $pdfoutput
+git commit -m "compiled $basename $modifydate"
 git push
