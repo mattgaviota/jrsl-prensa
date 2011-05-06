4b623d3e486b23bc969c5e66e2a47ccdf3af08a7
Carlos Cabrera <point_to_null@esdebian.org>
2011-05-06 05:32:36 -0300

    establecida versión compilada pero sin header como mensaje

--- compile.sh
+++ compile.sh
@@ -67,7 +67,7 @@ wkhtmltopdfoptions="\
 $wkhtmltopdf $wkhtmltopdfoptions "$htmloutput" "$pdfoutput"
 
 echo "Preparando posible envío masivo"
-cp $input mensaje.txt
+cp awk '/^\*\*/{pub=1}pub' $compiledrst > mensaje.txt
 cp $htmloutput mensaje.html
 
 echo "Publicando documentos en el sitio"
